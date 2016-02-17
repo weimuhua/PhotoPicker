1d527380a21427500f9d0c855f6a1cb34abf9448
weimuhua <weimuhua0812@126.com>
2016-02-17 20:58:42 +0800

    change : add Toolbar in PickPhotoActivity.

--- app/src/main/java/me/com/photopicker/activity/PickPhotoActivity.java
+++ app/src/main/java/me/com/photopicker/activity/PickPhotoActivity.java
@@ -3,11 +3,16 @@ package me.com.photopicker.activity;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 
@@ -26,6 +31,7 @@ public class PickPhotoActivity extends AppCompatActivity {
 
     private Context mContext;
 
+    private Toolbar mToolbar;
     private RecyclerView mRecyclerView;
     private ThumbnailAdapter mAdapter;
     private List<PhotoDir> mPhotoDirList;
@@ -56,9 +62,34 @@ public class PickPhotoActivity extends AppCompatActivity {
         initData();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_pick_photo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_finish) {
+            finish();
+        } else if (id == android.R.id.home) {
+            finish();
+        }
+        return true;
+    }
+
     private void initView() {
         mRecyclerView = (RecyclerView) findViewById(R.id.photo_recycler_view);
         mRecyclerView.setLayoutManager(new GridLayoutManager(mContext, 3));
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(getString(R.string.pick_photos));
+        mToolbar.setTitleTextColor(ContextCompat.getColor(this, android.R.color.white));
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     private void initData() {
